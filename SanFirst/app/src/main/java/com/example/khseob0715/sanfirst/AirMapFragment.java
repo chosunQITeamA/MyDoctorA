5364c203f25a12c98df12bd501d9213c2dd84599
JH_Kim <kimjh6980@naver.com>
2018-01-30 17:22:01 -0800

    GPS Location get success

--- SanFirst/app/src/main/java/com/example/khseob0715/sanfirst/AirMapFragment.java
+++ SanFirst/app/src/main/java/com/example/khseob0715/sanfirst/AirMapFragment.java
@@ -1,12 +1,19 @@
 package com.example.khseob0715.sanfirst;
 
-
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,19 +22,21 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static android.location.LocationManager.GPS_PROVIDER;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AirMapFragment extends Fragment implements OnMapReadyCallback
-{
+public class AirMapFragment extends Fragment implements OnMapReadyCallback {
     private MapView mapView = null;
 
-    public AirMapFragment()
-    {
+    Double lat = 37.56;
+    Double lon = 126.97;
+
+    GoogleMap map;
+    public AirMapFragment() {
         // required
     }
-
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,8 +47,7 @@ public class AirMapFragment extends Fragment implements OnMapReadyCallback
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_air_map, container, false);
-
-        mapView = (MapView)layout.findViewById(R.id.map);
+        mapView = (MapView) layout.findViewById(R.id.map);
         mapView.getMapAsync(this);
 
         return layout;
@@ -90,32 +98,90 @@ public class AirMapFragment extends Fragment implements OnMapReadyCallback
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
         //액티비티가 처음 생성될 때 실행되는 함수
-
-        if(mapView != null)
-        {
+        if (mapView != null) {
             mapView.onCreate(savedInstanceState);
         }
     }
 
+    private void StartLocationService() {
+        LocationManager manager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+        GPSListener gpsListener = new GPSListener();
+        long minTime = 10000;
+        float minDistance = 0;
+        try {   //GPS 위치 요청
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            manager.requestLocationUpdates(GPS_PROVIDER, minTime, minDistance, (LocationListener) gpsListener);
+
+            // location request with network
+            manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minTime, minDistance, (LocationListener) gpsListener);
+
+            Location lastLocation = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if (lastLocation != null) {
+                Double latitude = lastLocation.getLatitude();
+                Double longitude = lastLocation.getLongitude();
+
+                Toast.makeText(getActivity(), "Lat:"+latitude + " / Lon:" + longitude, Toast.LENGTH_SHORT).show();
+            }
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(getActivity(), "start Location tracker.", Toast.LENGTH_SHORT).show();
+    }
+
+
+    private class GPSListener implements LocationListener{
+        @Override
+        public void onLocationChanged(Location location) {
+            lat = location.getLatitude();
+            lon = location.getLongitude();
+            String msg = "Lat:" + lat + " / Lon" + lon;
+            Toast.makeText(getActivity(), "My Location : " + lat + "/" + lon, Toast.LENGTH_SHORT).show();
+            ShowMyLocaion(lat, lon, map);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    }
+
+    private void ShowMyLocaion(Double lat, Double lon, GoogleMap googleMap) {
+        LatLng nowLocation = new LatLng(lat, lon);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(nowLocation);
+        markerOptions.title("now location");
+        //markerOptions.snippet("수도");
+        googleMap.addMarker(markerOptions);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(nowLocation));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(13));
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng SEOUL = new LatLng(37.56, 126.97);
 
-        MarkerOptions markerOptions = new MarkerOptions();
-
-        markerOptions.position(SEOUL);
-
-        markerOptions.title("서울");
-
-        markerOptions.snippet("수도");
-
-        googleMap.addMarker(markerOptions);
-
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-
-        googleMap.animateCamera(CameraUpdateFactory.zoomTo(13));
+        map = googleMap;
+        StartLocationService();
     }
 
 }
