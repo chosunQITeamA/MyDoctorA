414c76da3969c5319af76455562f0e67bdc21614
Han Seob Kim <khseob0715@gmail.com>
2018-02-01 13:20:40 -0800

    fragment onclick method

--- SanFirst/app/src/main/java/com/example/khseob0715/sanfirst/AccountFragment.java
+++ SanFirst/app/src/main/java/com/example/khseob0715/sanfirst/AccountFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -23,7 +25,27 @@ public class AccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_account, container, false);
+
+        Button deleteBtn = (Button)rootView.findViewById(R.id.idDeleteBtn);
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(getContext(),"complete",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button updateBtn = (Button)rootView.findViewById(R.id.updateBtn);
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"complete",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        return rootView;
     }
 
 }
--- SanFirst/app/src/main/java/com/example/khseob0715/sanfirst/ProfileFragment.java
+++ SanFirst/app/src/main/java/com/example/khseob0715/sanfirst/ProfileFragment.java
@@ -1,11 +1,16 @@
 package com.example.khseob0715.sanfirst;
 
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -13,6 +18,8 @@ import android.view.ViewGroup;
  */
 public class ProfileFragment extends Fragment {
 
+    Button BirthSelectBtn;
+    DatePickerDialog datePickerDialog;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -23,7 +30,25 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_profile, container, false);
+
+        BirthSelectBtn = (Button)rootView.findViewById(R.id.Birth_select);
+        BirthSelectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog.OnDateSetListener callback = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        BirthSelectBtn.setText(year + "/" + (monthOfYear+1) + "/" + dayOfMonth);
+                    }
+                };
+
+                datePickerDialog = new DatePickerDialog(view.getContext(), android.R.style.Theme_Material_Light_Dialog_Alert, callback, 2018, 0, 19);
+                datePickerDialog.show();
+            }
+        });
+        return rootView;
+
     }
 
 }
