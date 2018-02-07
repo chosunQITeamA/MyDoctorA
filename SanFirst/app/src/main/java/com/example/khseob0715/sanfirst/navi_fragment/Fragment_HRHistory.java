1a3cfd1c83e71b3736cadfc990014cefd136852b
khseob0715 <khseob0715@naver.com>
2018-02-06 16:35:34 -0800

    historical heart-related data list view ui

--- SanFirst/app/src/main/java/com/example/khseob0715/sanfirst/navi_fragment/Fragment_HRHistory.java
+++ SanFirst/app/src/main/java/com/example/khseob0715/sanfirst/navi_fragment/Fragment_HRHistory.java
@@ -1,10 +1,13 @@
 package com.example.khseob0715.sanfirst.navi_fragment;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +16,7 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.example.khseob0715.sanfirst.Activity.UserMainActivity;
 import com.example.khseob0715.sanfirst.R;
-import com.lylc.widget.circularprogressbar.CircularProgressBar;
-
-import java.util.List;
 
 
 /**
@@ -28,16 +27,16 @@ public class Fragment_HRHistory extends Fragment {
     ListView listView;
     myAdapter adapter;
 
-    String[] items = {"ss","das"};
+    String[] items = {"ss", "das"};
     ViewGroup rootView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       rootView = (ViewGroup)inflater.inflate(R.layout.fragment_heartrate_history, container, false);
+        rootView = (ViewGroup) inflater.inflate(R.layout.fragment_heartrate_history, container, false);
 
-        listView = (ListView)rootView.findViewById(R.id.listView);
+        listView = (ListView) rootView.findViewById(R.id.listView);
         adapter = new myAdapter();
         listView.setAdapter(adapter);
 
@@ -48,7 +47,8 @@ public class Fragment_HRHistory extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
     }
-    class myAdapter extends BaseAdapter{
+
+    class myAdapter extends BaseAdapter {
 
         @Override
         public int getCount() {
@@ -67,10 +67,30 @@ public class Fragment_HRHistory extends Fragment {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            TextView view = new TextView(rootView.getContext());
-            view.setText(items[position]);
-            view.setTextSize(40.0f);
+            HistoricalHeart view = new HistoricalHeart(rootView.getContext());
+            //view.setText(items[position]);
+            //view.setTextSize(40.0f);
             return view;
         }
+    }
+
+    class HistoricalHeart extends LinearLayout {
+        TextView Test;
+
+        public HistoricalHeart(Context context) {
+            super(context);
+            init(context);
+        }
+
+        public HistoricalHeart(Context context, @Nullable AttributeSet attrs) {
+            super(context, attrs);
+            init(context);
+        }
+
+        private void init(Context context) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            inflater.inflate(R.layout.historical_heart_list, this);
+        }
+
     }
 }
