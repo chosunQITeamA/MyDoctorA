c5b1d913a6e8c45a3287fb2a1e3f875af4622dc0
JH_Kim <kimjh6980@naver.com>
2018-02-09 10:40:55 -0800

    Udoo BT Conn

--- SanFirst/app/src/main/java/com/example/khseob0715/sanfirst/udoo_btchat/BluetoothAQI.java
+++ SanFirst/app/src/main/java/com/example/khseob0715/sanfirst/udoo_btchat/BluetoothAQI.java
@@ -8,22 +8,18 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
- * Created by enemf on 2018-02-04.
+ * Created by chony on 2018-02-04.
  */
-
 public class BluetoothAQI extends Service{
 
     private static final String TAG = "BluetoothService";
-    // Intent request codes
-    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
-    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
-    private static final int REQUEST_ENABLE_BT = 3;
-    /**
-     * Name of the connected device
-     */
-    private String mConnectedDeviceName = null;
+
+    public final static String ACTION_SEND_AQI_DATA =
+            "com.example.jeonghoon.googlemap.bluetooth.ACTION_SEND_AQI_DATA";
+
 
     /**
      * String buffer for outgoing messages
@@ -38,48 +34,91 @@ public class BluetoothAQI extends Service{
     @Override
     public void onCreate(){
         super.onCreate();
-        Log.i("BluetoothService","onCreate");
+        Log.i(TAG,"onCreate");
+
 
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        Log.i("BluetoothService", "onBind");
+        Log.i(TAG, "onBind");
         return null;
     }
 
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
-        Log.i("BluetoothService", "onStartCommand");
+        Log.i(TAG, "onStartCommand");
         bluetoothIntent=intent;
         try {
             address = bluetoothIntent.getStringExtra(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
 
-            Log.i("BluetoothService", "address : "+address);
+            Log.i(TAG, "address : "+address);
 
             mBluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
             if(mBluetoothAdapter==null)onDestroy();
 
-            BluetoothDevice device=mBluetoothAdapter.getRemoteDevice(address);
+            if(!address.equals(" have been paired")){
+                BluetoothDevice device=mBluetoothAdapter.getRemoteDevice(address);
 
-            mChatService = new BluetoothChatService(getApplicationContext(), mHandler);
-            mOutStringBuffer = new StringBuffer("");
-
-            mChatService.connect(device, true);
-
-            setupChat();
-            if (mChatService != null) {
-                // Only if the state is STATE_NONE, do we know that we haven't started already
-                if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
-                    // Start the Bluetooth chat services
-                    mChatService.start();
+                if (mChatService == null) {
+                    setupChat();
                 }
+                mChatService.connect(device, true);
+
+                if (mChatService != null) {
+                    // Only if the state is STATE_NONE, do we know that we haven't started already
+                    if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
+                        Log.i(TAG, "state : "+mChatService.getState());
+                        // Start the Bluetooth chat services
+                        mChatService.start();
+                    }
+                }
+                setupChat();
+            }else{
+                Toast.makeText(this, "Please turn on the Bluetooth",Toast.LENGTH_SHORT).show();
             }
         }catch(NullPointerException nex){
-            Log.e(TAG, "Exception : "+nex.getMessage());
+            Log.e(TAG, "Exception_onStart : "+nex.getMessage());
         }
+/*
+        int count=0;
+        String sendData="";
+        while(count<=3){
+            double ran=Math.random()*500;
+            sendData=String.valueOf(ran);
+            for(int i=0;i<8;i++){
+                ran=Math.random()*500;
+                sendData=sendData+","+String.valueOf(ran);
+            }
+            Log.i(TAG,"data : "+sendData);
+            broadcastUpdate(ACTION_SEND_AQI_DATA, sendData);
+            SystemClock.sleep(1000);
+            count++;
+        }
+*/
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void sendMessage(String message) {//메시지 전송
+        // Check that we're actually connected before trying anything
+        // 연결재확인
+        if (mChatService.getState() != BluetoothChatService.STATE_CONNECTED) {
+            Log.d(TAG, "state : "+mChatService.getState());
+            return;
+        }
+
+        // Check that there's actually something to send
+        if (message.length() > 0) {
+            // Get the message bytes and tell the BluetoothChatService to write
+            System.getProperty("line.separator");
+            byte[] send = (message.replace("[\r\n]","")+"\n").getBytes();
+            mChatService.write(send);//메시지 전송
+
+            // Reset out string buffer to zero and clear the edit text field
+            //에딧 텍스트 클리어&리스트에 메시지 추가
+            mOutStringBuffer.setLength(0);
+        }
     }
 
     @Override
@@ -88,7 +127,7 @@ public class BluetoothAQI extends Service{
         if(mChatService!=null){
             mChatService.stop();
         }
-        Log.i("BluetoothService","onDestroy");
+        Log.i(TAG,"onDestroy");
     }
 
     /**
@@ -98,11 +137,20 @@ public class BluetoothAQI extends Service{
         Log.d(TAG, "setupChat()");
 
         // Initialize the BluetoothChatService to perform bluetooth connections
-        mChatService = new BluetoothChatService(getApplicationContext(), mHandler);
+        if(mChatService==null)
+            mChatService = new BluetoothChatService(getApplicationContext(), mHandler);
 
         // Initialize the buffer for outgoing messages
         mOutStringBuffer = new StringBuffer("");
+
     }
+    public void broadcastUpdate(final String action, String data) {
+        final Intent intent = new Intent();
+        intent.setAction(action);
+        intent.putExtra(ACTION_SEND_AQI_DATA, data);
+        sendBroadcast(intent);
+    }
+
 
     private final Handler mHandler = new Handler() {
         @Override
@@ -111,64 +159,50 @@ public class BluetoothAQI extends Service{
                 case Constants.MESSAGE_STATE_CHANGE:
                     switch (msg.arg1) {
                         case BluetoothChatService.STATE_CONNECTED:
-                            Log.i(TAG, "Connected!!!"+mChatService.getState());
+                            Log.i(TAG, "Connected!!! state : "+mChatService.getState());
 
-                            if (mChatService.getState() != BluetoothChatService.STATE_CONNECTED) {
-                                Log.e(TAG, "not_connected!!!");
-                                return;
-                            }
+                            String message="Server connected!";
+                            String sendData="sensor";
 
-                            System.getProperty("line.separator");
+                            /*for(int i=0;i<9;i++){
+                                ran=Math.random()*500;
+                                sendData=sendData+","+String.valueOf(ran);
+                            }*/
+                            Log.i(TAG,"data : "+sendData);
 
-                            byte[] send=("If u recv this msg, plz kakaotalk to me.".replace("[\r\n]", "")+"\n").getBytes();
+                            byte[] send = (sendData.replace("[\r\n]","")+"\n").getBytes();
                             mChatService.write(send);
-
-                            mOutStringBuffer.setLength(0);
+                            Toast.makeText(getApplicationContext(),"Connected",Toast.LENGTH_SHORT).show();
                             break;
                         case BluetoothChatService.STATE_CONNECTING:
                             Log.i(TAG, "Connecting!!!");
+                            Toast.makeText(getApplicationContext(),"Connecting",Toast.LENGTH_SHORT).show();
                             break;
                         case BluetoothChatService.STATE_LISTEN:
                         case BluetoothChatService.STATE_NONE:
+                            Log.i(TAG, "NoState!!!");
+                            Toast.makeText(getApplicationContext(),"Can't Connect",Toast.LENGTH_SHORT).show();
                             break;
                     }
                     break;
                 case Constants.MESSAGE_WRITE://쓰기
                     byte[] writeBuf = (byte[]) msg.obj;
-                    Log.i(TAG, "Write!!!");
                     // construct a string from the buffer
-                    //String writeMessage = new String(writeBuf);
-                    //mConversationArrayAdapter.add(writeMessage);
+                    String writeMessage = new String(writeBuf);
+                    Log.i(TAG, "Write!!!"+writeMessage);
                     break;
                 case Constants.MESSAGE_READ://읽기
                     byte[] readBuf = (byte[]) msg.obj;
                     // construct a string from the valid bytes in the buffer
                     String readMessage = new String(readBuf, 0, msg.arg1);
-                    Log.i(TAG, "Read!!!"+readMessage);
-                    //mConversationArrayAdapter.add(mConnectedDeviceName + " : " + readMessage);
-/*
-                    String[] BTSplit = readMessage.split(",");  // Split Message (split word : ,)
-                    int temperatureval = Integer.valueOf(BTSplit[0]);   // receive temperatureval
+                    broadcastUpdate(ACTION_SEND_AQI_DATA, readMessage);
 
-                    int coval = Integer.valueOf(BTSplit[1]);
-                    int so2val = Integer.valueOf(BTSplit[2]);
-                    int o3val = Integer.valueOf(BTSplit[3]);
-                    int no2val = Integer.valueOf(BTSplit[4]);
-                    int pm25val = Integer.valueOf(BTSplit[5]);
-                    Log.e("Udoo", temperatureval +"/"+coval+"/"+so2val+"/"+o3val+"/"+no2val+"/"+pm25val);
-                    */
-/*
-                    UserActivity.udoo_temperature = temperatureval;
-                    UserActivity.udoo_co = coval;
-                    UserActivity.udoo_so2 = so2val;
-                    UserActivity.udoo_o3 = o3val;
-                    UserActivity.udoo_no2 = no2val;
-                    UserActivity.udoo_pm25 = pm25val;
-*/
+                    Log.i(TAG, "Read!!!"+readMessage);
                     break;
                 default:
                     break;
             }
         }
     };
+
 }
